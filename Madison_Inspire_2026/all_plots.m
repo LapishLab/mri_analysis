04e126a81569f055598fa42d20110ca2ab76718b
David Swygart <davidswygart@gmail.com>
2026-04-28 16:20:36 -0400

    add preference plots

--- Madison_Inspire_2026/all_plots.m
+++ Madison_Inspire_2026/all_plots.m
@@ -65,22 +65,18 @@ nan_mean = @(y) mean(y, "omitmissing");
 is_IAP = day_info.duration==1440;
 x = day_info.day(is_IAP);
 
-% y-axis = preference (0-1)
-    y_lab = 'Ethanol preference';
-    y = preference(:, is_IAP);
-    y_lim = [0 1];
-% % y-axis = ethanol intake (mg/kg)
-    % y_lab = 'Ethanol intake (mg/kg)';
-    % y = E_per_kg(:, is_IAP);
-    % y_lim = [0 10];
 
-% Plotting
-figure(11); clf;
+% Plotting Intake
+y_lab = 'Ethanol intake (mg/kg)';
+y = E_per_kg(:, is_IAP);
+y_lim = [0 10];
+
+figure(1); clf;
 % male vs female
 subplot(2,1,1); hold on
 shadedErrorBar(x, y(is_male,:), {nan_mean, sem}, 'lineProps', {'Color',m_color, 'DisplayName', 'Male'})
 shadedErrorBar(x, y(~is_male,:), {nan_mean, sem}, 'lineProps', {'Color',f_color, 'DisplayName', 'Female'})
-legend()
+% legend()
 xlabel('Days')
 ylabel(y_lab)
 ylim(y_lim)
@@ -90,7 +86,37 @@ subplot(2,1,2); hold on
 shadedErrorBar(x, y(is_p,:), {nan_mean, sem}, 'lineProps', {'Color',p_color, 'DisplayName', 'P'})
 shadedErrorBar(x, y(is_wis,:), {nan_mean, sem}, 'lineProps', {'Color',w_color, 'DisplayName', 'Wistar'})
 shadedErrorBar(x, y(is_had,:), {nan_mean, sem}, 'lineProps', {'Color',h_color, 'DisplayName', 'HAD1'})
-legend()
+% legend()
+xlabel('Days')
+ylabel(y_lab)
+ylim(y_lim)
+xlim([0 80])
+saveas(gcf, 'fig1.svg')
+
+
+
+
+% Plotting prefrence
+y_lab = 'Ethanol preference';
+y = preference(:, is_IAP);
+y_lim = [0 1];
+
+figure(11); clf;
+% male vs female
+subplot(2,1,1); hold on
+shadedErrorBar(x, y(is_male,:), {nan_mean, sem}, 'lineProps', {'Color',m_color, 'DisplayName', 'Male'})
+shadedErrorBar(x, y(~is_male,:), {nan_mean, sem}, 'lineProps', {'Color',f_color, 'DisplayName', 'Female'})
+% legend()
+xlabel('Days')
+ylabel(y_lab)
+ylim(y_lim)
+xlim([0 80])
+% Wistar vs P vs HAD1
+subplot(2,1,2); hold on
+shadedErrorBar(x, y(is_p,:), {nan_mean, sem}, 'lineProps', {'Color',p_color, 'DisplayName', 'P'})
+shadedErrorBar(x, y(is_wis,:), {nan_mean, sem}, 'lineProps', {'Color',w_color, 'DisplayName', 'Wistar'})
+shadedErrorBar(x, y(is_had,:), {nan_mean, sem}, 'lineProps', {'Color',h_color, 'DisplayName', 'HAD1'})
+% legend()
 xlabel('Days')
 ylabel(y_lab)
 ylim(y_lim)
@@ -105,37 +131,64 @@ is_transition(find(is_IAP, 6, 'last')) = true; % Also add last 1 day of IAP
 x = day_info.day(is_transition);
 % x = x - x(2)
 
-% y-axis = preference (0-1)
-    y_lab = 'Ethanol preference';
-    y = preference(:, is_transition);
-    y_lim = [0 1];
-% % y-axis = ethanol intake (mg/kg/hr)
-    % y_lab = 'Ethanol intake (mg/kg)';
-    % y = E_per_kg(:, is_transition);
-    % y_lim = [0 10];
 
-% Plotting
+% Plotting intake
+y_lab = 'Ethanol intake (mg/kg)';
+y = E_per_kg(:, is_transition);
+y_lim = [0 10];
+
+
 figure(2); clf;
 % male vs female
 subplot(2,1,1); hold on
 shadedErrorBar(x, y(is_male,:), {nan_mean, sem}, 'lineProps', {'Color',m_color, 'DisplayName', 'Male'})
 shadedErrorBar(x, y(~is_male,:), {nan_mean, sem}, 'lineProps', {'Color',f_color, 'DisplayName', 'Female'})
-legend()
+% legend()
 xlabel('Days')
 ylabel(y_lab)
 ylim(y_lim)
+xline (82, '--k')
+% Wistar vs P vs HAD1
+subplot(2,1,2); hold on
+shadedErrorBar(x, y(is_p,:), {nan_mean, sem}, 'lineProps', {'Color',p_color, 'DisplayName', 'P'})
+shadedErrorBar(x, y(is_wis,:), {nan_mean, sem}, 'lineProps', {'Color',w_color, 'DisplayName', 'Wistar'})
+shadedErrorBar(x, y(is_had,:), {nan_mean, sem}, 'lineProps', {'Color',h_color, 'DisplayName', 'HAD1'})
+% legend()
+xlabel('Days')
+ylabel(y_lab)
+ylim(y_lim)
+xline (82, '--k')
+
+saveas(gcf, 'fig2.svg')
+
+% Plotting preference
+y_lab = 'Ethanol preference';
+y = preference(:, is_transition);
+y_lim = [0 1];
+
+figure(22); clf;
+% male vs female
+subplot(2,1,1); hold on
+shadedErrorBar(x, y(is_male,:), {nan_mean, sem}, 'lineProps', {'Color',m_color, 'DisplayName', 'Male'})
+shadedErrorBar(x, y(~is_male,:), {nan_mean, sem}, 'lineProps', {'Color',f_color, 'DisplayName', 'Female'})
+% legend()
+xlabel('Days')
+ylabel(y_lab)
+ylim(y_lim)
+xline (82, '--k')
 
 % Wistar vs P vs HAD1
 subplot(2,1,2); hold on
 shadedErrorBar(x, y(is_p,:), {nan_mean, sem}, 'lineProps', {'Color',p_color, 'DisplayName', 'P'})
 shadedErrorBar(x, y(is_wis,:), {nan_mean, sem}, 'lineProps', {'Color',w_color, 'DisplayName', 'Wistar'})
 shadedErrorBar(x, y(is_had,:), {nan_mean, sem}, 'lineProps', {'Color',h_color, 'DisplayName', 'HAD1'})
-legend()
+% legend()
 xlabel('Days')
 ylabel(y_lab)
 ylim(y_lim)
+xline (82, '--k')
 
-saveas(gcf, 'fig2.svg')
+saveas(gcf, 'fig22.svg')
 %% %%%%%%%%%% Figure 3: Quinine Sensitivity (compulsivity) %%%%%%%%%%%%%
 
 % X-groups are baseline (last 4 days of regular LAP), low-quinine, and high-quinine
@@ -149,19 +202,19 @@ is_high_quinine = day_info.duration==20 & day_info.quinine==740;
 % per condition (accross days)
 baseline = mean(E_per_kg(:, is_baseline), 2, "omitmissing");
 low_quinine = mean(E_per_kg(:, is_low_quinine), 2, "omitmissing");
-low_quinine = (1 -low_quinine ./ baseline) * 100;
+low_quinine = (low_quinine ./ baseline) * 100;
 
 high_quinine = mean(E_per_kg(:, is_high_quinine), 2, "omitmissing");
-high_quinine = (1 - high_quinine ./ baseline) * 100;
+high_quinine = (high_quinine ./ baseline) * 100;
 
-y_lab = 'Quinine sensitivity (%)';
+y_lab = 'Consumption (%)';
 
 saveas(gcf, 'fig3.svg')
 %% Plotting Low Qhinine
 figure(3); clf;
 sgtitle("Low-quinine")
 y = low_quinine;
-y_lim = [-50 150];
+y_lim = [0 150];
 
 % male vs female
 subplot(2,1,1); hold on
@@ -170,6 +223,7 @@ y_cell = {y(is_male), y(~is_male)};
 raw_data_error_bar(x, y_cell, bar_funcs={nan_mean, sem},bar_color={m_color, f_color})
 ylabel(y_lab)
 ylim(y_lim)
+yline(100, '--k')
 
 % Wistar vs P vs HAD1
 subplot(2,1,2); hold on
@@ -178,12 +232,14 @@ y_cell = {y(is_wis), y(is_p), y(is_had)};
 raw_data_error_bar(x, y_cell, bar_funcs={nan_mean, sem}, bar_color={w_color, p_color, h_color})
 ylabel(y_lab)
 ylim(y_lim)
+yline(100, '--k')
+
 saveas(gcf, 'fig3.svg')
 %% Plotting high Quinine
 figure(4); clf;
 sgtitle("High-quinine")
 y = high_quinine;
-y_lim = [-50 150];
+y_lim = [0 100];
 
 % male vs female
 subplot(2,1,1); hold on
@@ -192,6 +248,7 @@ y_cell = {y(is_male), y(~is_male)};
 raw_data_error_bar(x, y_cell, bar_funcs={nan_mean, sem}, bar_color={m_color, f_color})
 ylabel(y_lab)
 ylim(y_lim)
+yline(100, '--k')
 
 % Wistar vs P vs HAD1
 subplot(2,1,2); hold on
@@ -200,4 +257,5 @@ y_cell = {y(is_wis), y(is_p), y(is_had)};
 raw_data_error_bar(x, y_cell, bar_funcs={nan_mean, sem}, bar_color={w_color, p_color, h_color})
 ylabel(y_lab)
 ylim(y_lim)
+yline(100, '--k')
 saveas(gcf, 'fig4.svg')
